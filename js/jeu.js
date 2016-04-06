efac31893f5d4e0f051d31e3f02db8dffab9c33d
crira <matt.chazelot@yahoo.fr>
2016-04-06 09:50:39 +0200

    rien

--- js/jeu.js
+++ js/jeu.js
@@ -4,11 +4,8 @@ window.onload  = function () {
     compteur = setInterval('prog();',100);
 }
 
-<<<<<<< HEAD
-var largeurGrille  = 3; // largeur en nombre de gros carré (mettre un nombre impaire sinon on pourra avoir des égalités et on a pas envie)
-=======
+
 var largeurGrille  = 7; // largeur en nombre de gros carré (mettre un nombre impaire sinon on pourra avoir des égalités et on a pas envie)
->>>>>>> origin/master
 
 var joueurActif = 0; //retient le joueur actif
 var scoreJ1=0;
